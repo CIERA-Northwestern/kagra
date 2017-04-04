abed954d24a67d73227dd2719a9b38a07c511e9b
eachase <eve.adde.chase@gmail.com>
2017-04-04 17:57:59 -0500

    scatter, run_scatter, make_plots: link to new fits files; mildly corrected outlier determination

--- scripts/run_scatter.sh
+++ scripts/run_scatter.sh
@@ -7,14 +7,14 @@ source /projects/b1011/ligo_project/lsc/lalinference_o2/etc/lscsoftrc
 
 # FIXME: several of these labels are for "fixed" runs without actually pointing to fixed runs
 
-# Fixed injection HKLV
-python scatter.py --underplot network -p "HLVK_fixed=/projects/b1011/kagra/kagra_o2_lalinference/skymap/HLVK/*/post/*.fits.gz" -c "HLVK_fixed=snr,hsv" --inj-xml /projects/b1011/spinning_runs/IMRfreezinginj.xml -x "HKLV"
+# Fixed injection HKLV -- change this
+#python scatter.py --underplot network -p "HLVK_fixed=/projects/b1011/kagra/HLVK/skymap/*/*.fits.gz" -c "HLVK_fixed=snr,hsv" --inj-xml /projects/b1011/spinning_runs/IMRfreezinginj.xml -x "HKLV"
 
 # Fixed injection HLV (no Kagra)
-python scatter.py --underplot network -p "HLV_fixed=/projects/b1011/kagra/kagra_o2_lalinference/skymap/HLV/*/none/post/*.fits.gz" -c "HLV_fixed=snr,hsv" --inj-xml /projects/b1011/spinning_runs/IMRfreezinginj.xml -x "HLV" -n "H1 L1 V1"
+python scatter.py --underplot network -p "HLV_fixed=/projects/b1011/kagra/HLV/*/none/post/*.fits.gz" -c "HLV_fixed=snr,hsv" --inj-xml /projects/b1011/spinning_runs/IMRfreezinginj.xml -x "HLV" -n "H1 L1 V1"
 
-# Fixed injection HILKV 
-python scatter.py --underplot network -p "HLVKI_fixed=/projects/b1011/kagra/kagra_o2_lalinference/skymap/HLVKI/*/*.fits.gz" -c "HLVKI_fixed=snr,hsv" --inj-xml /projects/b1011/spinning_runs/IMRfreezinginj.xml -x "HIKLV" -n "H1 L1 V1 K1 I1"
+# Fixed injection HILKV
+#python scatter.py --underplot network -p "HLVKI_fixed=/projects/b1011/kagra/HLVKI/skymap/*/*.fits.gz" -c "HLVKI_fixed=snr,hsv" --inj-xml /projects/b1011/spinning_runs/IMRfreezinginj.xml -x "HIKLV" -n "H1 L1 V1 K1 I1"
 
 
 
