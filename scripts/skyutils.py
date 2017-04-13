ca35c0f8346ebcf89bd75fb3edbc29112d5052c4
Chris Pankow <chris.pankow@ligo.org>
2017-04-13 12:54:20 -0500

    skyutils.py: add timing and err angle estimate functions

--- scripts/skyutils.py
+++ scripts/skyutils.py
@@ -1,11 +1,113 @@
+import copy
+import itertools
+
 import healpy
 import numpy as np
 from matplotlib import pyplot as plt
-from lal import LIGOTimeGPS, GreenwichMeanSiderealTime, ComputeDetAMResponse
 
+from lal import LIGOTimeGPS, GreenwichMeanSiderealTime, ComputeDetAMResponse
 import lal
-detectors = lal.CachedDetectors
-detectors = dict([(d.frDetector.prefix, d) for d in detectors])
+detectors = dict([(d.frDetector.prefix, d) for d in lal.CachedDetectors])
+
+# Some reference numbers from Fairhurst 2009
+# SNR of 10, up to ISCO
+_aLIGO_delta_t = 0.1e-3
+_aVirgo_delta_t = 0.1e-3
+_timing = {
+    "H1": _aLIGO_delta_t,
+    "L1": _aLIGO_delta_t,
+    "K1": _aLIGO_delta_t,
+    "I1": _aLIGO_delta_t,
+    "V1": _aVirgo_delta_t
+}
+def get_timing_dict(snr=10, dets=None):
+    """
+    Get a dictionary with the \sigma_t values for each detector specified by 'dets'. If 'dets' is not specified, all five HKILV will be retrieved. The timing errors are scaled by signal to noise ratio, and pinned to an SNR of 10.
+    """
+    new_dict = copy.copy(_timing)
+    for d in copy.copy(new_dict.keys()):
+        if dets is not None and d not in dets:
+            del new_dict[d]
+            continue
+        # Timing uncertainty (uncorrelated) scales like 1/snr
+        new_dict[d] *= 10. / snr
+    return new_dict
+
+def iter_dets(dets, n=4, unique=True):
+    for comb in itertools.product(*([dets] * n)):
+        if unique and len(set(comb)) < n - 1:
+            continue
+        yield comb
+
+def baseline(d1, d2):
+    r1 = detectors[d1].location
+    r2 = detectors[d2].location
+    return r1 - r2
+
+def source_vec_from_pos(ra, dec):
+    """
+    RA, DEC -> normalized position vector
+    TODO: This probably should be negative.
+    """
+    #return np.asarray((np.sin(dec) * np.cos(ra), np.sin(dec) * np.sin(ra), np.cos(dec)))
+    return np.asarray((np.cos(dec) * np.cos(ra), np.cos(dec) * np.sin(ra), np.sin(dec)))
+
+def source_pos_from_vec(x, y, z):
+    """
+    normalized position vector -> RA, DEC
+    """
+    r = np.sqrt(x**2 + y**2 + z**2)
+    th = 0 if r == 0 else np.arccos(z / r)
+    phi = np.arctan2(y, x)
+    return np.asarray((th, phi))
+
+def solid_angle_error(source_pos, network_timing):
+    """
+    Schutz, 2011, eqn. 31 --- quoted from earlier Wen and Chen.
+    """
+    del_omega_inv_sq = np.zeros(np.shape(source_pos)[-2:])
+    source_vec = source_vec_from_pos(*source_pos)
+    for det_comb in iter_dets(network_timing.keys()):
+        timing = map(network_timing.__getitem__, det_comb)
+        # NOTE: We reverse the components because of the indexing, but doesn't
+        # really matter once we take the magnitude
+        r_1 = baseline(det_comb[1], det_comb[0])
+        r_2 = baseline(det_comb[3], det_comb[2])
+        det_plane = np.cross(r_1, r_2)
+        # FIXME: Bad, don't know why
+        det_plane *= -1
+        del_omega_inv_sq += np.prod(timing)**-2 * np.dot(det_plane, np.swapaxes(source_vec, 1, 0))**2
+    timing_sum = np.sum(1. / np.power(network_timing.values(), 2))
+
+    return 4 * np.sqrt(2) * np.pi * lal.C_SI**2 * timing_sum / np.sqrt(del_omega_inv_sq)
+
+def delay_to_plane(source_pos, network_timing):
+    """
+    Schutz, 2011, eqn. 31 --- quoted from earlier Wen and Chen.
+    """
+    del_omega_inv_sq = np.zeros(np.shape(source_pos)[-2:])
+    source_vec = source_vec_from_pos(*source_pos)
+    for det_comb in iter_dets(network_timing.keys()):
+        # NOTE: We reverse the components because of the indexing, but doesn't
+        # really matter once we take the magnitude
+        r_1 = baseline(det_comb[1], det_comb[0])
+        r_2 = baseline(det_comb[3], det_comb[2])
+        if np.all(np.cross(r_1, r_2) == 0):
+            continue
+        r_1 /= np.sqrt(np.dot(r_1, r_1))
+        r_2 /= np.sqrt(np.dot(r_2, r_2))
+        det_plane = np.cross(r_1, r_2)
+        det_plane /= np.sqrt(np.dot(det_plane, det_plane))
+        # TODO: Bad, don't know why
+        det_plane *= -1
+        ip = []
+        _tmp = np.asarray(source_pos).reshape(2, -1)
+        for i, vec in enumerate(source_vec.reshape(3, -1).T):
+            ip.append(np.dot(det_plane, vec))
+        del_omega_inv_sq += np.dot(det_plane, np.swapaxes(source_vec, 1, 0))
+        break
+
+    return del_omega_inv_sq
 
 def create_table(fitsfile):
     skymap = healpy.read_map(fitsfile)
